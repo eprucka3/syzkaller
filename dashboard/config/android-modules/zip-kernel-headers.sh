f98e564f9274e356fd33bfd5114cfc82c9e2434a
Liz Prucka <lizprucka@google.com>
2022-11-28 17:41:54 +0000

    pkg/build, dashboard/config/android-modules: build android-5.10-cuttlefish
    Builds Android kernel for Cuttlefish with the passed in params.Config file
    (currently android-5.10-cuttlefish.config).
    
    Additional constants added to Make commands:
    The environment variable 'BUILD_GOLDFISH_DRIVERS' included is used by
    kbuild to add the goldfish drivers.
    The prebuilts path contains necessary prebuilts and is pre-appended to the
    path in the kernel build script.
    
    Create-initramfs.sh bundles the initramfs file.
    Zip-kernel-headers.sh zips the kernel headers.
    
    Overall steps:
    1) Build common kernel (create bzImage and prepare for modules)
    2) Install modules
    3) Bundle initramfs and zip kernel headers

---
+++ dashboard/config/android-modules/zip-kernel-headers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright 2021 syzkaller project authors. All rights reserved.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+# Zips kernel headers given a root kernel directory.
+
+# $1 Kernel common dir
+KERNEL_COMMON_DIR=$1
+KERNEL_HEADERS_TAR=${KERNEL_COMMON_DIR}/kernel-headers.tar.gz
+echo " Copying kernel headers to ${KERNEL_HEADERS_TAR}"
+pushd $KERNEL_COMMON_DIR
+  find arch include $KERNEL_COMMON_DIR -name *.h -print0               \
+          | tar -czf $KERNEL_HEADERS_TAR                     \
+            --absolute-names                                 \
+            --dereference                                    \
+            --transform "s,.*$KERNEL_COMMON_DIR,,"                     \
+            --transform "s,^,kernel-headers/,"               \
+            --null -T -
+popd
